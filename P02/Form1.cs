7dd60a30d7f45ab9fc57f066e85a6b7ebf41810b
Filip Dusil <dusilf.05@spst.eu>
2022-03-31 11:31:48 +0200

    2-done,3-done,4

--- P02/Form1.cs
+++ P02/Form1.cs
@@ -29,14 +29,14 @@ namespace P02
                hodnota = (int) i;
                 if( i >= 'a' && i <= 'z')
                 {
-                    if (i != 'z')
-                    {
+                  
                         hodnota += posun;
-                    }
-                    else
-                    {
-                        hodnota = (int)('a') + posun;
-                    }
+                    
+                   
+                }
+                if ((char)hodnota > 'z')
+                {
+                    hodnota = hodnota - 26;
 
                 }
 
@@ -47,6 +47,42 @@ namespace P02
 
             }
             textBox2.Text = vysledek;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string veta = textBox2.Text;
+            int posun = Convert.ToInt32(textBox4.Text);
+            string vysledek = "";
+            int hodnota = 0;
+            char pismeno = ' ';
+            foreach (char i in veta)
+            {
+                hodnota = (int)i;
+                if (i >= 'a' && i <= 'z')
+                {
+
+                    hodnota -= posun;
+
+
+                }
+                if ((char)hodnota < 'a')
+                {
+                    hodnota = hodnota + 26;
+
+                }
+
+
+                pismeno = (char)hodnota;
+                vysledek += pismeno;
+
+
+            }
+
+            textBox3.Text = vysledek;
+
+
         }
     }
 }
---
+++ P03/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string veta = textBox1.Text;
+            char pocetsmazani = ' ';
+            int prom = 0;
+
+            foreach (char i in textBox1.Text)
+            {
+                if (i >= '0' && i <= '9')
+                {
+                    pocetsmazani = i;
+                    prom = 1;
+                    break;
+                }
+            }
+            if (prom == 0)
+            {
+
+                textBox2.Text = veta;
+            }
+            else
+            {
+                int pocet = veta.Length;
+                int odkud = pocet - ((int)pocetsmazani - 48);
+                int pocets = ((int)pocetsmazani - 48);
+
+                if (pocets > pocet)
+                {
+                    textBox2.Text = " ";
+                }
+                else
+                {
+                    string vysledek = veta.Remove(odkud, pocets);
+
+
+                    textBox2.Text = vysledek;
+                }
+            }
+        }
+    }
+}
