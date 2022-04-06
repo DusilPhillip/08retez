73eeae1e8e9e720254ed74da2628caadb3a97076
Filip Dusil <dusilf.05@spst.eu>
2022-04-06 09:35:55 +0200

    P06

---
+++ P06/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string veta = textBox1.Text;
+            string bezmezer = veta.Trim();
+
+            int i = 0;
+            while( i < bezmezer.Length-1)
+            {
+                if (bezmezer[i] == ' ')
+                {
+                    if (bezmezer[i + 1] == ' ')
+                    {
+                        bezmezer = bezmezer.Remove(i, 1);
+                    }
+                    else { i++; }
+                }
+            }
+        }
+    }
+}
